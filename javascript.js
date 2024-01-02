a30644b1febc2dee74691d85309ea7fbe6c89a8a
ivette-merc <ivettemerc@gmail.com>
2024-01-02 18:57:55 -0500

    images and new assets added

--- javascript.js
+++ javascript.js
@@ -1,1 +1,30 @@
 console.log("Hello World");
+
+const form = document.querySelector("#form");
+const submitButton = document.querySelector("#submit");
+
+form.addEventListener("submit", (e) => {
+  submitButton.disabled = true;
+  e.preventDefault();
+  window.location.href = window.location.origin + "/success.html";
+});
+
+setTimeout(() => {
+  window.location.href = window.location.origin;
+}, 5000);
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
